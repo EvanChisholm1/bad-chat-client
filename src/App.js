50a495bc3e79e412b7d3bd7bab55a4885036d213
Evan Chisholm <elchisholm248@gmail.com>
2020-07-18 18:42:51 -0400

    got rid of unused useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import './App.css';
 import { IoContext } from './components/IoContext';
 import styled from 'styled-components';
